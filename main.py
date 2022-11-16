ecc1d2be3e898f794a24569bec84261b94a7a498
Kingsley <992309340@qq.com>
2022-11-16 16:31:04 +0800

    new attached

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-
+print("test")
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
