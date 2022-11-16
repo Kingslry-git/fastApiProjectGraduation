f29ecc53c2e7721257482318c413d604b7a9bca6
Kingsley <992309340@qq.com>
2022-11-16 21:58:41 +0800

    to add png

---
+++ tkinter_test.py
@@ -0,0 +1,85 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title('kdd')
+
+root.geometry('800x800-100+100')
+
+# width = root.winfo_screenwidth()
+# height = root.winfo_screenheight()
+# w, h = 500, 500
+# print(f'{width} + {height}')
+# widget(container, **options)
+# message.pack()
+# root.minsize(300, 500)
+# root.maxsize(600, 600)
+
+# transparent
+root.attributes('-alpha', 1)
+# put the window at the top of the stacking order
+root.attributes('-topmost', 1)
+# resize(bool, bool)
+root.resizable(1, 1)
+
+# root1 = tk.Tk()
+# root1.title('kdd1')
+# root1.geometry('200x150+30+30')
+# root1.lower()
+# only available on .ico
+
+
+# tk.Label(root, text="hello1").pack()
+# ttk.Label(root, text="hello2").pack()
+
+
+# label_dic = ttk.Label(root)
+# label_dic['text'] = "It is a test."
+# label_dic.pack()
+
+def button_clicked():
+    print("clicked")
+
+
+def select(option):
+    print(option)
+
+
+def printOUT():
+    ttk.Label(root, text="command").pack()
+
+
+def return_pressed(event):
+    print('Return key pressed.')
+
+
+def log(event):
+    print(event)
+
+
+# label = ttk.Label(root)
+# label.config(text="hello3")
+# label.pack()
+
+# command no ()
+# button0 = ttk.Button(root, text="hello4", command=printOUT)
+# button0.pack()
+
+# btn = ttk.Button(root, text='Save')
+# btn.bind('<Return>', return_pressed)
+# btn.bind('<Alt_L>', log, add='+')
+# btn.focus()
+# btn.pack(expand=True)
+
+# text = tk.StringVar()
+# textbox = ttk.Entry(root, textvariable=text)
+# textbox.pack()
+# text.get()
+# ttk.Label(root,text='hello world', font=('hack',)).pack(ipadx=10,ipady=10)
+
+imag1 = tk.PhotoImage('.png')
+image_label = ttk.Label(root, image=imag1, compound='top')
+image_label.pack()
+
+root.mainloop()
+# root1.mainloop()
